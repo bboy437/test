bd79bad7546f23084946a2ad9007634e8486edcd
bboy0037 <attapolprommalee437@gmail.com>
2020-09-15 09:46:42 +0700

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,11 +10,19 @@ export class HomeComponent implements OnInit {
 
   date = new FormControl(new Date());
   time: any;
+
+  test: boolean;
+  test1: boolean;
   
   constructor() { }
 
   ngOnInit(): void {
     this.time = '05:11 pm';
+  }
+
+  onchange(event) {
+    console.log(event)
+    console.log(this.test)
   }
 
 }
